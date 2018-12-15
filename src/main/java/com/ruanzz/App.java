b36c22b4fa8c900daec73ced57034bb92f638cc4
ruanzz <rzz1995@live.com>
2018-12-15 23:59:47 +0800

    Initial commit

---
+++ src/main/java/com/ruanzz/App.java
@@ -0,0 +1,13 @@
+package com.ruanzz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
