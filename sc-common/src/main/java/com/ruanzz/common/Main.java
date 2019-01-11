f23cbf86c8415a285705079061e28edbeef19d0f
ruanzz <rzz1995@live.com>
2019-01-11 23:52:55 +0800

    处理构建失败

---
+++ sc-common/src/main/java/com/ruanzz/common/Main.java
@@ -0,0 +1,13 @@
+package com.ruanzz.common;
+
+/**
+ * @author ruanzz
+ * @date 2019/1/11
+ * @description TODO
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ sc-item/sc-item-interface/src/main/java/com/ruanzz/pojo/Main.java
@@ -0,0 +1,13 @@
+package com.ruanzz.pojo;
+
+/**
+ * @author ruanzz
+ * @date 2019/1/11
+ * @description TODO
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
